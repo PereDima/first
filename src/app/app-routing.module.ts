b2ecbba83c292ab26208746b52e5f374a7207bd2
Dmytro Pereklita <74322013+PereDima@users.noreply.github.com>
2021-09-15 23:30:10 +0300

    add project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 
-const routes: Routes = [];
+import { MainComponent } from './main/main.component';
+import { SkillsComponent } from './skills/skills.component';
+import { NfnComponent } from './nfn/nfn.component';
+import { StudyComponent } from './study/study.component';
+import { WorkComponent } from './work/work.component';
+
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'skills', component: SkillsComponent},
+  {path: 'work', component: WorkComponent},
+  {path: 'study', component: StudyComponent},
+  {path: '**', component: NfnComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
